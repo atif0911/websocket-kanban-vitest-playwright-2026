98b0ccbbf22227f851c49a3baf89b12d4ed34903
atif0911 <sardaratifali2003@gmail.com>
2026-02-18 03:55:00 +0530

    CORS Error fixing

--- backend/server.js
+++ backend/server.js
@@ -130,4 +130,8 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Server is running successfully!");
+});
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
