ac7535730158ccf84075ac4ac00f9942b2fc373a
atif0911 <sardaratifali2003@gmail.com>
2026-02-18 04:12:07 +0530

    Error fixing

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const API_URL = process.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(API_URL, {
   autoConnect: false, //we connect manually when the app loads
